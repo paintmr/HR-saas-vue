d28899af64cea2d26552a69a188724a2afab837b
chujingxl <chujingxl@gmail.com>
2022-01-22 23:52:36 +0800

    员工管理-员工详情页-新建文件上传组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
+import ImageUpload from './ImageUpload'
 
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools) // 注册工具栏组件
     Vue.component('UploadExcel', UploadExcel) // 注册导入Excel组件
+    Vue.component('ImageUpload', ImageUpload) // 注册图片上传组件
   }
 }
